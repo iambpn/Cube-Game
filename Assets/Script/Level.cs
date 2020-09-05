13db2c5d9b277579759cd127065e117be7d765f7
Bipin Maharjan <bopon.maharjan@gmail.com>
2020-09-05 19:21:18 +0545

    Cube Game V1.0

---
+++ Assets/Script/Level.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text lvl = this.GetComponent<Text>();
+        lvl.text = SceneManager.GetActiveScene().name;
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody player;
+    public float forwardForce = 1000f;
+    public float sideWaysForce = 500f;
+
+    
+    private void FixedUpdate()
+    {
+        //forward force
+        player.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.AddForce(-sideWaysForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.AddForce(sideWaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (this.transform.position.y <= 0)
+        {
+            this.enabled = false;
+            FindObjectOfType<GameManager>().setIsGameEnded(true);
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle")
+        {
+            this.enabled = false;
+            FindObjectOfType<GameManager>().setIsGameEnded(true);
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
